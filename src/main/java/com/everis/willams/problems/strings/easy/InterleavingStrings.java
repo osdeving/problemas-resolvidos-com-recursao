836f72e922f71e30b80e287c045eda04397e2332
Willams S. de Sousa <44249322+osdeving@users.noreply.github.com>
2021-03-16 14:02:53 -0300

    Refatorado nomes e adicionado comentários. Algoritmo não modificado.

--- src/main/java/com/everis/willams/problems/strings/easy/InterleavingStrings.java
+++ src/main/java/com/everis/willams/problems/strings/easy/InterleavingStrings.java
@@ -3,33 +3,40 @@ package com.everis.willams.problems.strings.easy;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+Encontre todas as intercalações que podem ser formadas por duas strings onde a ordem dos caracteres são preservadas
+
+P.ex.: "AB" e "CD" resulta em: "ACDB", "ACBD", "CABD", "CDAB", "CADB", "ABCD"
+Nota: Strings como "BA** não respeitaria a ordem (AB), bem como "**DC" também não.
+
+ */
 public class InterleavingStrings {
-    public static void findInterleavingStrings(String current, String first, String second, Set<String> result) {
+    public static void findInterleavingStrings(String current, String stringA, String stringB, Set<String> result) {
 
         // insert current if the end of both strings is reached
-        if(first.length() == 0 && second.length() == 0) {
+        if(stringA.length() == 0 && stringB.length() == 0) {
             result.add(current);
             return;
         }
 
-        // if 'first' is not empty, append tis first char and recur for the remaining substring
-        if(!first.isEmpty()) {
-            findInterleavingStrings(current + first.charAt(0), first.substring(1), second, result);
+        // if 'stringA' is not empty, append its first char and recur for the remaining substring
+        if(!stringA.isEmpty()) {
+            findInterleavingStrings(current + stringA.charAt(0), stringA.substring(1), stringB, result);
         }
 
-        // if 'second' is not empty, append its first char and recur for the remaining substring
-        if(!second.isEmpty()) {
-            findInterleavingStrings(current + second.charAt(0), first, second.substring(1), result);
+        // if 'stringB' is not empty, append its first char and recur for the remaining substring
+        if(!stringB.isEmpty()) {
+            findInterleavingStrings(current + stringB.charAt(0), stringA, stringB.substring(1), result);
         }
     }
 
     public static void main(String[] args) {
-        String first = "ABC";
-        String second = "ACB";
+        String stringA = "AB";
+        String stringB = "CD";
 
         Set<String> result = new HashSet<>();
 
-        findInterleavingStrings("", first, second, result);
+        findInterleavingStrings("", stringA, stringB, result);
 
         System.out.println(result);
     }
